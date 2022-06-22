a315cfa095727371e2fdc804091df7b08e6bfa1a
Divarion_D <danil.dmitrievn@gmail.com>
2022-06-22 12:23:13 +0300

    задукоментировал и добавил ввод вывод

--- common.py
+++ common.py
@@ -3,10 +3,11 @@ import re
 import sqlite3
 import xml.etree.ElementTree as ET
 import zipfile
-
+import shutil
 import requests
 
 
+# It downloads data from a url, parses it, and saves it.
 class Data:
     def __init__(self):
         self.conn = sqlite3.connect('data.db')  # connect to the database
@@ -17,21 +18,47 @@ class Data:
         self.cur.execute('''CREATE TABLE IF NOT EXISTS data(id INTEGER PRIMARY KEY AUTOINCREMENT, id_job INTEGER, link TEXT, name TEXT, region TEXT, description TEXT, pubdate TEXT, salary TEXT, company TEXT, expire TEXT, jobtype TEXT, phone TEXT)''')
 
     def download_data(self, url):
-        data = Downloader_Data(url)
-        self.parse_data = data.file_data()
-        self.save_data()
+        """
+        It downloads data from a url, parses it, and saves it
+
+        :param url: The URL of the file you want to download
+        """
+        data_down = Downloader_Data(
+            url)  # create a new object of the Downloader_Data class
+        self.parse_data = data_down.file_data()  # parse the data
+        data_down.delete_tmp()  # delete the temporary file
+        self.save_data()  # save the data to a file
 
     def save_data(self):
+        """
+        It takes the data from the parse_data function and saves it to a file
+        """
         Parse_And_Save_Data(self.parse_data)
 
-    def search_data(self, search_text, table):
-        self.search = Search_Data(search_text, table)
-        self.search = self.search.return_data()
-        return self.search
+    def get_columns(self):
+        """
+        It gets the column names from the database and stores them in a list
+        :return: The column names of the table.
+        """
+        self.cur.execute("SELECT * FROM data")  # set cursor to the table
+        self.columns = [description[0]
+                        for description in self.cur.description]  # get the column names
+        return self.columns  # return the column names
+
+    def search_data(self, search_text, column_number, exact_search):
+        #find all data in the column_number that matches the search text
+        self.cur.execute("SELECT * FROM data WHERE " + self.columns[column_number] + " LIKE ?", (search_text,))
+        search_data = self.cur.fetchall()
+        if exact_search:
+            search_data = [i for i in search_data if i[column_number] == search_text]
+        else:
+            search_data = [i for i in search_data if search_text in i[column_number]]
+        return search_data
+        
 
 
+# It downloads a zip file from a url, extracts the zip file, and then parses the xml file
 class Downloader_Data(Data):
-    # download data from the web and save it to a file
     def __init__(self, url):
         Data.__init__(self)
         self.domain = re.search(r'(https?://[^/]+)', url).group(1)
@@ -44,58 +71,81 @@ class Downloader_Data(Data):
         self.__search_url()
 
     def __search_url(self):
+        """
+        It takes the data from the get_data variable, and searches for a link that ends in .zip
+        """
         regexp = re.compile('<a.*?href="(.*?)".*?>(.*?)</a>')
         data_url = regexp.findall(self.get_data)
         for i in data_url:
             regexp = re.compile(r"sites/default/files/.+?.zip")
             url = regexp.findall(i[0])
             if url:
-                self.file_url = self.domain + "/" + url[0]
-                self.__download_file()
+                self.file_url = self.domain + "/" + url[0]  # set the file url
+                self.__download_file()  # download the file
                 break
 
     def __download_file(self):
-        if not os.path.exists(self.tmp_dir):
-            os.makedirs(self.tmp_dir)
-        r = requests.get(self.file_url, stream=True)
-        with open(self.tmp_dir + '/' + self.down_data_name, 'wb') as f:
+        """
+        It downloads the file from the url, and then extracts it
+        """
+        if not os.path.exists(self.tmp_dir):  # if the temporary directory doesn't exist
+            os.makedirs(self.tmp_dir)  # create the temporary directory
+        r = requests.get(self.file_url, stream=True)  # download the file
+        with open(self.tmp_dir + '/' + self.down_data_name, 'wb') as f:  # save the file
+            # iterate through the file
             for chunk in r.iter_content(chunk_size=self.chunk_size):
-                if chunk:
-                    f.write(chunk)
-                    f.flush()
-            f.close()
-        self.__extract_file()
+                if chunk:  # if the chunk is not empty
+                    f.write(chunk)  # write the chunk to the file
+                    f.flush()  # flush the file
+            f.close()  # close the file
+        self.__extract_file()  # extract the file
 
     def __extract_file(self):
-
+        """
+        It downloads a zip file, extracts it, and then deletes the zip file
+        """
         with zipfile.ZipFile(self.tmp_dir + '/' + self.down_data_name, 'r') as zip_ref:
-            zip_ref.extractall(self.tmp_dir)
-            zip_ref.close()
+            zip_ref.extractall(self.tmp_dir)  # extract the zip file
+            zip_ref.close()  # close the zip file
+            # delete the zip file
             os.remove(self.tmp_dir + '/' + self.down_data_name)
+            # iterate through the files in the temporary directory
             for file in os.listdir(self.tmp_dir):
                 os.rename(os.path.join(self.tmp_dir, file),
-                          os.path.join(self.tmp_dir, self.name_data))
+                          os.path.join(self.tmp_dir, self.name_data))  # rename the file
 
     def delete_tmp(self):
+        """
+        It deletes the temporary directory if it exists
+        """
         if os.path.exists(self.tmp_dir):
-            os.rmdir(self.tmp_dir)
+            shutil.rmtree(self.tmp_dir)
+        
 
     def file_data(self):
-        data = ET.parse(self.tmp_dir + '/' + self.name_data)
-        data = data.getroot()
-        return data
+        """
+        It takes a file name, parses it, and returns the root of the parsed file
+        :return: The data from the xml file.
+        """
+        data = ET.parse(self.tmp_dir + '/' + self.name_data) # parse the file
+        data = data.getroot() # get the root of the parsed file
+        return data # return the data
 
 
+# It parses data from xml and saves it to sqlite
 class Parse_And_Save_Data(Data):
-    # parse data from xml and save to sqlite
     def __init__(self, data):
         Data.__init__(self)
         self.data = data
         self.__parse_data()
 
     def __parse_data(self):
+        """
+        It takes the XML data from the API, parses it, and saves it to a database
+        """
         for x in self.data:
             data = dict()  # create array to store data
+            # enter data into the array
             data['id_job'] = x.attrib.get('id')
             data['link'] = self.__parse_cdata(x.find('link'))
             data['name'] = self.__parse_cdata(x.find('name'))
@@ -107,12 +157,18 @@ class Parse_And_Save_Data(Data):
             data['expire'] = self.__parse_cdata(x.find('expire'))
             data['jobtype'] = self.__parse_cdata(x.find('jobtype'))
             data['phone'] = self.__parse_cdata(x.find('phone'))
-            self.parse_data = data
-            self.__save_data()
+            self.parse_data = data # set the parse_data variable
+            self.__save_data() # save the data to the database
         self.conn.commit()  # commit changes to database
         self.conn.close()  # close connection to database
 
     def __parse_cdata(self, data):
+        """
+        It takes a cdata tag and returns the text inside the tag
+
+        :param data: the data to be parsed
+        :return: The text inside the cdata tag.
+        """
         try:
             data = data.text  # get the text inside the cdata
         except AttributeError:
@@ -122,35 +178,16 @@ class Parse_And_Save_Data(Data):
         return data
 
     def __save_data(self):
-        # insert data to database
+        """
+        It takes the keys from the dictionary, creates a string of comma separated keys, then takes the
+        values from the dictionary, creates a string of comma separated question marks, then creates a
+        string of the SQL query, then takes the values from the dictionary, converts the boolean values
+        to integers, then executes the SQL query
+        """
         columns = ', '.join(self.parse_data.keys())  # get all keys from data
-        # get all placeholders from data
-        placeholders = ', '.join('?' * len(self.parse_data))
+        placeholders = ', '.join('?' * len(self.parse_data)) # get all placeholders from data
         sql = 'INSERT INTO data ({}) VALUES ({})'.format(
             columns, placeholders)  # create sql query
-        # get all values from data
         values = [int(x) if isinstance(x, bool)
-                  else x for x in self.parse_data.values()]
-        self.cur.execute(sql, values)  # execute sql query
-
-
-class Search_Data(Data):
-    def __init__(self, search_text, search_column):
-        Data.__init__(self)
-        self.cur.execute('''SELECT * FROM data''')
-        self.data = self.cur.fetchall()
-        self.search_text = search_text
-        self.search_column = search_column
-        self.__search_data()
-
-    def __search_data(self):
-        # get all column names from database
-        self.cur.execute('''SELECT * FROM data''')
-        self.columns = [column[0] for column in self.cur.description]
-        # get index of column name
-        self.index = self.columns.index(self.search_column)
-        # search data by column name
-        self.data = [i for i in self.data if self.search_text in i[self.index]]
-
-    def return_data(self):
-        return self.data
+                  else x for x in self.parse_data.values()] # get all values from data
+        self.cur.execute(sql, values)  # execute the query
--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from common import Data
 
 data = Data()
-data.download_data(
-    "https://mail2.dcz.gov.ua/publikaciya/6-perelik-aktualnyh-vakansiy-stanom-na-datu")
-data = data.search_data('програміст', 'name')
+data.download_data("https://mail2.dcz.gov.ua/publikaciya/6-perelik-aktualnyh-vakansiy-stanom-na-datu")
 
-with open('data.txt', 'w') as f:
-    for i in data:
-        f.write(str(i))
-        f.write('\n')
+columns = data.get_columns()
+
+while True:
+    print('Which columns to look for')
+    for i in range(len(columns)):
+        print(str(i) + ": " + columns[i])
+    column_number = input("Enter the number of the column you want to look for: ")
+    if column_number.isdigit():
+        column_number = int(column_number)
+        if column_number in range(len(columns)):
+            search_text = input("Enter the text you want to search for: ")
+            exact_search = input("Do you want to search for an exact match? (y/n): ")
+            if exact_search == "y":
+                exact_search = True
+            else:
+                exact_search = False
+            searc_data = data.search_data(search_text, column_number, exact_search)
+
+            with open('data.txt', 'w') as f:
+                for i in searc_data:
+                    f.write(str(i))
+                    f.write('\n')
+            f.close()
+            print("Done")
